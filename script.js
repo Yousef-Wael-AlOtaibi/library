286be9c78704c2b730071a4e7fda28a229f72ec7
Yousef Wael Al Otaibi <ywalotaibi2010@gmail.com>
2026-01-29 16:31:10 +0300

    Add book property to hold the book's html
    Using a function to create the book's HTML card or container and then
    assign the HTML the .card prop of the book so it can later be easily
    accessed and displayed.

--- script.js
+++ script.js
@@ -5,9 +5,31 @@ function Book(title, author, pages, description) {
     this.author = author;
     this.pages = pages;
     this.id = crypto.randomUUID();
+    this.description = description;
+    this.card = createBookCard(this);
 };
 
 function addBookToLibrary(title, author, pages, description) {
     const book = new Book(title, author, pages, description);
     library.push(book);
 };
+
+function createBookCard(book) {
+    const bookCard = document.createElement('div');
+    const bookTitle = document.createElement('h3');
+    bookTitle.textContent = book.title;
+    bookCard.append(bookTitle);
+
+    const bookAuthor = document.createElement('p');
+    bookAuthor.textContent = `By ${book.author}`;
+    bookCard.append(bookAuthor);
+
+    const bookDescription = document.createElement('p');
+    bookDescription.textContent = book.description;
+    bookCard.append(bookDescription);
+
+    const bookPageCount = document.createElement('p');
+    bookPageCount.textContent = `${book.pages} Page(s)`;
+    bookCard.append(bookPageCount);
+    return bookCard;
+}
